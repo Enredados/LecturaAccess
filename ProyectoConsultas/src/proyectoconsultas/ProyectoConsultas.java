d5b75b1d015a914013c21e6f63bf4571c076dd9f
AlanJuker <75257308+AlanJuker@users.noreply.github.com>
2021-05-25 15:33:52 -0500

    Todo el programa xd

---
+++ ProyectoConsultas/src/proyectoconsultas/ProyectoConsultas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoconsultas;
+
+/**
+ *
+ * @author alang
+ */
+public class ProyectoConsultas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new ventanaConsulta().setVisible(true);
+    }
+    
+}
